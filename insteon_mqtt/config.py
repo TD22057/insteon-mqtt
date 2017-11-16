1205c6a7a0ec6177a90343af854757a9e765d1bd
Ted Drain <ted.drain@gmail.com>
2017-11-16 15:04:22 -0800

    Added comments

--- insteon_mqtt/config.py
+++ insteon_mqtt/config.py
@@ -1,16 +1,17 @@
 #===========================================================================
 #
-# Insteon-MQTT bridge Python package
+# Configuration file utiltiies.
 #
 #===========================================================================
 
-__doc__ = """TODO: doc"""
+__doc__ = """Configuration file utilties
+"""
 
 #===========================================================================
 import functools
 from . import device
 
-# Possible devices we can read and build from the config file.
+# Configuration file input description to class map.
 devices = {
     'dimmer' : device.Dimmer,
     'motion' : device.Motion,
@@ -19,12 +20,27 @@ devices = {
     'mini_remote8' : functools.partial(device.Remote, num=8),
     }
 
+
 #===========================================================================
 def find(name):
+    """Find a device class from a description.
+
+    Valid inputs are defined in the config.devices dictionary.
+
+    Raises:
+      Exception if the input device is unknown.
+
+    Args:
+      name:   (str) The device type name.
+
+    Returns:
+      Returns the device class to use for the input.
+    """
     name = name.lower()
     dev = devices.get(name, None)
     if not dev:
-        raise Exception("Unknown device name '%s'" % name)
+        raise Exception("Unknown device name '%s'.  Valid names are "
+                        "%s." % (name, devices.keys()))
 
     return dev
 
