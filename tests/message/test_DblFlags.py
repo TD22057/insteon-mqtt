8e409f28f7082881b30c3143aca7e638c303d548
Ted Drain <ted.drain@gmail.com>
2017-11-17 18:21:23 -0800

    Added unit tests

---
+++ tests/message/test_DblFlags.py
@@ -0,0 +1,50 @@
+#===========================================================================
+#
+# Tests for: insteont_mqtt/message/DbFlags.py
+#
+#===========================================================================
+import insteon_mqtt.message as Msg
+import pytest
+from pytest import assume
+
+#===========================================================================
+class Test_DbFlags:
+    #-----------------------------------------------------------------------
+    def check(self, obj, in_use, is_ctrl, is_high, raw):
+        assert(obj.in_use == in_use)
+        assert(obj.is_controller == is_ctrl)
+        assert(obj.is_responder != obj.is_controller)
+        assert(obj.high_water == is_high)
+
+        out = obj.to_bytes()
+        assert(len(out) == 1)
+        assert(out[0] == raw)
+
+        d = obj.to_json()
+        obj2 = Msg.DbFlags.from_json(d)
+        assert(obj2.in_use == in_use)
+        assert(obj2.is_controller == is_ctrl)
+        assert(obj2.high_water == is_high)
+
+        str(obj)
+
+    #-----------------------------------------------------------------------
+    def test_basic(self):
+        use_ctrl_high = 0b11000010
+        use_ctrl_low = 0b11000000
+        use_resp_high = 0b10000010
+        free_ctrl_high = 0b01000010
+        free_ctrl_low = 0b01000000
+        free_resp_high = 0b00000010
+
+        b = bytes([use_ctrl_high, use_ctrl_low, use_resp_high,
+                   free_ctrl_high, free_ctrl_low, free_resp_high])
+        in_use = [1, 1, 1, 0, 0, 0]
+        is_ctrl = [1, 1, 0, 1, 1, 0]
+        is_high = [1, 0, 1, 1, 0, 1]
+
+        for i in range(len(b)):
+            obj = Msg.DbFlags.from_bytes(b, i)
+            self.check(obj, in_use[i], is_ctrl[i], is_high[i], b[i])
+
+    #-----------------------------------------------------------------------
---
+++ tests/message/test_Flags.py
@@ -0,0 +1,49 @@
+#===========================================================================
+#
+# Tests for: insteont_mqtt/message/Flags.py
+#
+#===========================================================================
+import insteon_mqtt.message as Msg
+import pytest
+from pytest import assume
+
+#===========================================================================
+class Test_DbFlags:
+    #-----------------------------------------------------------------------
+    def check(self, obj, type, is_ext, hops, max_hops, nak, broadcast, raw):
+        assert(obj.type == type)
+        assert(obj.is_ext == is_ext)
+        assert(obj.hops_left == hops)
+        assert(obj.max_hops == max_hops)
+        assert(obj.is_nak == nak)
+        assert(obj.is_broadcast == broadcast)
+
+        out = obj.to_bytes()
+        assert(len(out) == 1)
+        assert(out[0] == raw)
+
+        str(obj)
+
+    #-----------------------------------------------------------------------
+    def test_basic(self):
+        b = bytes([
+            0b00000011,
+            0b00110110,
+            0b01001001,
+            0b01111100,
+            0b10000011,
+            0b10110110,
+            0b11001001,
+            0b11111100,
+            ])
+        type = [0, 1, 2, 3, 4, 5, 6, 7]
+        ext = [0, 1, 0, 1, 0, 1, 0, 1]
+        hops = [0, 1, 2, 3, 0, 1, 2, 3]
+        max_hops = [3, 2, 1, 0, 3, 2, 1, 0]
+
+        for i in range(len(b)):
+            obj = Msg.Flags.from_bytes(b, i)
+            self.check(obj, type[i], ext[i], hops[i], max_hops[i],
+                       type[i] == 5 or type[i] == 7, type[i] == 6, b[i])
+
+    #-----------------------------------------------------------------------
