b95f8c6f0ba12792065ef4392c9f428ec72bbdd5
Kevin Keegan <kevin@krkeegan.com>
2019-01-07 18:59:33 -0800

    Parse Config Scenes into DB_Config
    This only deals with the device level db, not the modem yet.
    
    Handles three variations of config file defintions.
    
    Adds the concept of data_1, data_2, and data_3 keys in a definition.

--- insteon_mqtt/db/DeviceEntry.py
+++ insteon_mqtt/db/DeviceEntry.py
@@ -21,6 +21,22 @@ class DeviceEntry:
     entry.
 
     The entry can be converted to/from JSON with to_json() and from_json().
+
+    Notes on the values for Data 1-3 in links:
+      Responder Records
+        Data 1    Link-specific data (e.g. On-Level)
+        Data 2    Link-specific data (e.g. Ramp Rates, Setpoints, etc.)
+        Data 3    Link-specific data (listed by Insteon as "normally unused"
+                             but for multi-function items, we know that this
+                             is set to the linked "group" on the responding
+                             device)
+
+      Controller Records
+        Data 1    Number of retries (Normally set to 03, FF = no retries,
+                  00 = Broadcast for cleanup)
+        Data 2    Listed as Ignored?
+        Data 3    Listed as 00 for switchlinc type devices and 01-08 for KPL
+                  type devices
     """
 
     @staticmethod
