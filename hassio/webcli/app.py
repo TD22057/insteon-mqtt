dfcf3fc26fd08cb80e8aef7670dfadbca2c3b016
KRKeegan <kevin@krkeegan.com>
2020-12-04 16:24:17 -0800

    An Functional Implementation with a Reasonable Front End
    There are almost no guardrails here.  So a user could do
    something stupid.

--- hassio/webcli/app.py
+++ hassio/webcli/app.py
@@ -1,27 +1,51 @@
-import subprocess
-from flask import Flask, Response
 import sys
+import time
+import subprocess
+from shlex import split
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 
 cli = sys.modules['flask.cli']
 cli.show_server_banner = lambda *x: None
-app = Flask(__name__)
+app = Flask(__name__, template_folder=".")
+app.config['output'] = None
+app.config['cmd'] = []
+socketio = SocketIO(app)
 
 @app.route('/')
+def index():
+    return render_template("index.html")
+
+@socketio.on('message')
+def handle_message(message):
+    command = ['../../insteon-mqtt', '../../config.yaml']
+    command.extend(split(message))
+    app.config['cmd'].append(command)
+
+@socketio.on('connect')
+def test_connect():
+    # If already defined, then skip
+    if app.config["output"]:
+        return
+    socketio.start_background_task(target=insteon_mqtt_webcli)
+
 def insteon_mqtt_webcli():
-    def stream():
-        output = subprocess.Popen(["../insteon-mqtt", "../config.yaml", "-h"],
-                                  text=True,
-                                  stdout=subprocess.PIPE,
-                                  stderr=subprocess.STDOUT)
-        yield "<pre style='white-space: pre-wrap;'>"
-        line = output.stdout.readline()
-        while line:
-            yield line
-            line = output.stdout.readline()
-        yield "</pre>"
-    return  Response(stream())
+    while True:
+        if len(app.config['cmd']):
+            command = app.config['cmd'].pop()
+            socketio.emit('message', "-->" + " ".join(command) + "\n")
+            app.config["output"] = subprocess.Popen(command,
+                                                    text=True,
+                                                    stdout=subprocess.PIPE,
+                                                    stderr=subprocess.STDOUT)
+            line = app.config["output"].stdout.readline()
+            while line:
+                socketio.emit('message', line)
+                line = app.config["output"].stdout.readline()
+        else:
+            time.sleep(.1)
 
 if __name__ == '__main__':
     # host='172.30.32.2' for hass ingest
-    app.run(host='127.0.0.1', port='8099')
+    socketio.run(app, host='127.0.0.1', port='8099')
