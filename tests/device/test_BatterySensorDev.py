486868d6643a4f21447e5ca171aef9e93e04151c
KRKeegan <kevin@krkeegan.com>
2020-12-23 11:15:44 -0800

    Add Tests for Awake and Pop_Queue

--- tests/device/test_BatterySensorDev.py
+++ tests/device/test_BatterySensorDev.py
@@ -2,15 +2,16 @@
 #
 # Tests for: insteont_mqtt/device/BatterySensor.py
 #
+# pylint: disable=W0621,W0212
 #===========================================================================
-import pytest
-# from pprint import pprint
 from unittest import mock
 from unittest.mock import call
+import pytest
+# from pprint import pprint
 import insteon_mqtt as IM
-import insteon_mqtt.device.BatterySensor as BatterySensor
+import insteon_mqtt.device as Device
 import insteon_mqtt.message as Msg
-import insteon_mqtt.util as util
+# import insteon_mqtt.util as util
 import helpers as H
 
 @pytest.fixture
@@ -21,13 +22,13 @@ def test_device(tmpdir):
     protocol = H.main.MockProtocol()
     modem = H.main.MockModem(tmpdir)
     addr = IM.Address(0x01, 0x02, 0x03)
-    device = BatterySensor(protocol, modem, addr)
+    device = Device.BatterySensor(protocol, modem, addr)
     return device
 
 
 class Test_Base_Config():
     def test_pair(self, test_device):
-        with mock.patch.object(IM.CommandSeq, 'add'):
+        with mock.patch.object(IM.CommandSeq, 'add') as mocked:
             test_device.pair()
             calls = [
                 call(test_device.refresh),
@@ -38,8 +39,8 @@ class Test_Base_Config():
                 call(test_device.db_add_ctrl_of, 0x04, test_device.modem.addr, 0x01,
                      refresh=False),
             ]
-            IM.CommandSeq.add.assert_has_calls(calls)
-            assert IM.CommandSeq.add.call_count == 4
+            mocked.assert_has_calls(calls)
+            assert mocked.call_count == 4
 
     @pytest.mark.parametrize("cmd1,expected", [
         (Msg.CmdType.ON, True),
@@ -47,7 +48,7 @@ class Test_Base_Config():
         (Msg.CmdType.LINK_CLEANUP_REPORT, None),
     ])
     def test_broadcast_1(self, test_device, cmd1, expected):
-        with mock.patch.object(BatterySensor, '_set_is_on') as mocked:
+        with mock.patch.object(Device.BatterySensor, '_set_is_on') as mocked:
             flags = Msg.Flags(Msg.Flags.Type.ALL_LINK_BROADCAST, False)
             group = IM.Address(0x00, 0x00, 0x01)
             addr = IM.Address(0x01, 0x02, 0x03)
@@ -91,3 +92,45 @@ class Test_Base_Config():
                 mocked.assert_called_once_with(test_device, expected)
             else:
                 mocked.assert_not_called()
+
+    def test_pop_queue_and_awake1(self, test_device):
+        # test with empty queue
+        test_device._pop_send_queue()
+        assert len(test_device.protocol.sent) == 0
+        #Queue a message
+        msg = Msg.OutStandard.direct(test_device.addr, 0x11, 0xff)
+        msg_handler = IM.handler.StandardCmd(msg, None, None)
+        test_device.send(msg, msg_handler)
+        # Message hasn't been sent and is queued
+        assert len(test_device.protocol.sent) == 0
+        assert len(test_device._send_queue) == 1
+        # Pretend we got a message from the device and none already sending
+        test_device.protocol.addr_in_queue = False
+        test_device._pop_send_queue()
+        assert len(test_device.protocol.sent) == 1
+        assert len(test_device._send_queue) == 0
+        assert msg_handler._num_retry == 0
+
+    def test_pop_queue_and_awake2(self, test_device):
+        #Queue a message
+        msg = Msg.OutStandard.direct(test_device.addr, 0x11, 0xff)
+        msg_handler = IM.handler.StandardCmd(msg, None, None)
+        test_device.send(msg, msg_handler)
+        # Pretend we got a message from the device and one already sending
+        test_device.protocol.addr_in_queue = True
+        test_device._pop_send_queue()
+        assert len(test_device.protocol.sent) == 0
+        assert len(test_device._send_queue) == 1
+
+    def test_pop_queue_and_awake3(self, test_device):
+        #Queue a message
+        msg = Msg.OutStandard.direct(test_device.addr, 0x11, 0xff)
+        msg_handler = IM.handler.StandardCmd(msg, None, None)
+        test_device.send(msg, msg_handler)
+        # Pretend we called awake
+        def on_done(*args):
+            pass
+        test_device.awake(on_done)
+        assert len(test_device.protocol.sent) == 1
+        assert len(test_device._send_queue) == 0
+        assert msg_handler._num_retry > 0
