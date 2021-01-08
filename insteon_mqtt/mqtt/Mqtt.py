ca506b7f7160655b89639b88cefddb06d321011c
KRKeegan <kevin@krkeegan.com>
2021-01-08 15:37:10 -0800

    Subscribe to MQTT On New Device if Already Connected
    A small bug that just worked until I changed how the startup
    sequence ran.  Now that the loop runs before everything is loaded
    new devices were not being subscribed since the MQTT link was
    already up and running.

--- insteon_mqtt/mqtt/Mqtt.py
+++ insteon_mqtt/mqtt/Mqtt.py
@@ -179,6 +179,10 @@ class Mqtt:
         # Save the MQTT device so we can find it again.
         self.devices[device.addr.id] = obj
 
+        # If we are already connected we need to subscribe this device
+        if self.link.connected:
+            obj.subscribe(self.link, self.qos)
+
     #-----------------------------------------------------------------------
     def handle_cmd(self, client, userdata, message):
         """MQTT command message callback.
