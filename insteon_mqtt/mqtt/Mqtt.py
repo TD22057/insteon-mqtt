93f45d087f72d7fb18a63bdc246d174b6b760db6
Kevin Robert Keegan <kevin@krkeegan.com>
2021-01-09 11:57:03 -0800

    MQTT Subscribe Only After MQTT Connect Callback
    The return from network/mqtt.connect() does not describe whether
    the MQTT connection has been established.  This uses the
    on_connect callback instead.  The subscribe calls will only be
    made after on_connect signals a valid connection.
    
    This Closes #312

--- insteon_mqtt/mqtt/Mqtt.py
+++ insteon_mqtt/mqtt/Mqtt.py
@@ -146,7 +146,7 @@ class Mqtt:
           link (network.Mqtt):  The MQTT network link.
           connected (bool):  True if connected, False if disconnected.
         """
-        if connected:
+        if self.link.connected:
             self._subscribe()
 
     #-----------------------------------------------------------------------
@@ -180,7 +180,8 @@ class Mqtt:
         self.devices[device.addr.id] = obj
 
         # If we are already connected we need to subscribe this device
-        obj.subscribe(self.link, self.qos)
+        if self.link.connected:
+            obj.subscribe(self.link, self.qos)
 
     #-----------------------------------------------------------------------
     def handle_cmd(self, client, userdata, message):
