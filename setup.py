2c65da6a510a32f175706fa5993f672a107fd483
KRKeegan <kevin@krkeegan.com>
2021-05-04 16:19:32 -0700

    Bump version: 0.8.3 → 0.9.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ test_requirements = open("requirements-test.txt").readlines()
 
 setuptools.setup(
     name = 'insteon-mqtt',
-    version = '0.8.3',
+    version = '0.9.0',
     description = "Insteon <-> MQTT bridge server",
     long_description = readme,
     author = "Ted Drain",
