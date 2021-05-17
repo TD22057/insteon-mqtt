394f18964dcf4720fd42b99f41df9dec5cc1a4b2
Lloyd Ramey <lnr0626@users.noreply.github.com>
2021-05-17 11:41:10 -0400

    fix issue with hassio initial config setup

--- hassio/entrypoint.sh
+++ hassio/entrypoint.sh
@@ -6,7 +6,7 @@
 
 if [ ! -f /config/insteon-mqtt/config.yaml ]; then
     echo "Copying default config.yaml"
-    /bin/cp /opt/insteon-mqtt/config.yaml /config/insteon-mqtt/config.yaml
+    /bin/cp /config/insteon-mqtt/config.yaml.default /config/insteon-mqtt/config.yaml
     sed -i "s/storage: 'data'/storage: '\/config\/insteon-mqtt\/data'/" /config/insteon-mqtt/config.yaml
     sed -i "s/file: \/var\/log\/insteon_mqtt.log/file: \/config\/insteon-mqtt\/insteon_mqtt.log/" /config/insteon-mqtt/config.yaml
 fi
