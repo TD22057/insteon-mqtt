4f417699b8739636a88c2d97c66dee03ec442937
Lloyd Ramey <lnr0626@gmail.com>
2021-05-17 16:47:12 -0400

    fix flake8 issues

--- insteon_mqtt/device/base/DimmerBase.py
+++ insteon_mqtt/device/base/DimmerBase.py
@@ -167,12 +167,14 @@ class DimmerBase(ManualCtrl, ResponderBase, Base):
         # 0x05 - Disables resume dim
         if resume_dim:
             LOG.info("Device %s enabling resume dim", self.label)
-            cmd2=0x04
+            cmd2 = 0x04
         else:
             LOG.info("Device %s disabling resume dim", self.label)
-            cmd2=0x05
+            cmd2 = 0x05
 
-        msg = Msg.OutExtended.direct(self.addr, Msg.CmdType.SET_OPERATING_FLAGS, cmd2, bytes([0x00] * 14))
+        cmd1 = Msg.CmdType.SET_OPERATING_FLAGS
+        msg = Msg.OutExtended.direct(self.addr, cmd1, cmd2,
+                                      bytes([0x00] * 14))
 
         # Use the standard command handler which will notify us when the
         # command is ACK'ed.
