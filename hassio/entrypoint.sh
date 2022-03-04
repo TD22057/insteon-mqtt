850e83a96964228af6867af12ba94f8fa868baee
Kevin Robert Keegan <kevin@krkeegan.com>
2022-03-03 16:26:51 -0800

    Fix Path in Description
    Closes #485

--- hassio/entrypoint.sh
+++ hassio/entrypoint.sh
@@ -10,7 +10,7 @@ if [ ! -f /config/insteon-mqtt/config.yaml ]; then
     /bin/cp /config/insteon-mqtt/config.yaml.default /config/insteon-mqtt/config.yaml
     sed -i "s/#storage: 'data'/storage: '\/config\/insteon-mqtt\/data'/" /config/insteon-mqtt/config.yaml
     sed -i "s/#file: \/var\/log\/insteon_mqtt.log/file: \/config\/insteon-mqtt\/insteon_mqtt.log/" /config/insteon-mqtt/config.yaml
-    echo "Please edit the file /config/insteon_mqtt/config.yaml"
+    echo "Please edit the file /config/insteon-mqtt/config.yaml"
     echo "Then you can start InsteonMQTT."
 else
     python3 /opt/insteon-mqtt/hassio/start.py /config/insteon-mqtt/config.yaml start
