2a4aac4f759b1797fcaf84f1c1478700a4fb3b7b
Kevin Robert Keegan <kevin@krkeegan.com>
2023-08-16 14:41:15 -0700

    Fix Flake8 Errors

--- insteon_mqtt/device/base/ResponderBase.py
+++ insteon_mqtt/device/base/ResponderBase.py
@@ -114,11 +114,11 @@ class ResponderBase(Base):
         LOG.info("Device %s grp: %s cmd: on %s", self.addr, group, mode)
         assert group in self.responder_groups
         if isinstance(mode, str):
-          try:
-            mode = on_off.Mode(mode.lower())
-          except:
-              LOG.error("Invalid mode string '%s'", mode)
-              mode = on_off.Mode.NORMAL
+            try:
+                mode = on_off.Mode(mode.lower())
+            except:
+                LOG.error("Invalid mode string '%s'", mode)
+                mode = on_off.Mode.NORMAL
         assert isinstance(mode, on_off.Mode)
 
         # Send the requested on code value.
@@ -157,11 +157,11 @@ class ResponderBase(Base):
         LOG.info("Device %s grp: %s cmd: off %s", self.addr, group, mode)
         assert group in self.responder_groups
         if isinstance(mode, str):
-          try:
-            mode = on_off.Mode(mode.lower())
-          except:
-              LOG.error("Invalid mode string '%s'", mode)
-              mode = on_off.Mode.NORMAL
+            try:
+                mode = on_off.Mode(mode.lower())
+            except:
+                LOG.error("Invalid mode string '%s'", mode)
+                mode = on_off.Mode.NORMAL
         assert isinstance(mode, on_off.Mode)
 
         # Send an off or instant off command.
@@ -384,7 +384,7 @@ class ResponderBase(Base):
         """
         # I am not sure I am aware of increment group commands.  Is there
         # some way I can cause one to occur?
-        pass # pragma: no cover
+        pass  # pragma: no cover
 
     #-----------------------------------------------------------------------
     def group_cmd_handle_manual(self, manual, group, reason):
@@ -400,4 +400,4 @@ class ResponderBase(Base):
           group (int):  The local db entry for this group command.
           reason (str): Whether the command was ON or OFF
         """
-        pass # pragma: no cover
+        pass  # pragma: no cover
